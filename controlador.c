aa6ef110e250d2ebeb4bd6e69857e6f5aee9dbba
Francisco Costa <xicon73@gmail.com>
2017-05-25 15:07:11 +0100

    initiated controlador

--- controlador.c
+++ controlador.c
@@ -1,4 +1,7 @@
-int main() {
-return 0;
-}
-
+#include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
+#include <fcntl.h> /*File control -> O_RDONLY, O_WRONLY, O_CREAT, O_* */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#define PIPE_BUF 100
